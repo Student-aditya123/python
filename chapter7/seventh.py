8f0ac3ab745a1f3ff08ed38439dbef804f855e06
Aditya Singh <adityasingh606104@gmail.com>
2025-11-08 21:47:09 +0530

    complete chapter7 and chapter8

--- chapter7/seventh.py
+++ chapter7/seventh.py
@@ -1,2 +1,100 @@
 # <<<<<<<<<<Loops In Python>>>>>>>>>>
 # 1. Write a program to print multiplication table of a given number using for loop. 
+# n = int(input("Enter the number: "))
+# for i in range(1, 11):
+#       print(f"2 X {i} = {n * i}")
+
+#  2. Write a program to greet all the person names stored in a list ‘l’ and which starts 
+# with S. 
+# l = ["Harry", "Soham", "Sachin", "Rahul"]     
+# for item in l:
+    # for ch in item:
+    #     if ch=="S":
+    #         print(f"greet:",item)
+# this is not perfect for because when there is in name 'S' present other than
+#  starting then it greet many time to that name.
+#********2nd solution********
+# l = ["Harry", "Soham", "Sachin", "Rahul", "Saraswati"] 
+# for item in l:
+#     if item.startswith("S"):
+#        print("greet: ",item)
+
+# 3. Attempt problem 1 using while loop. 
+# n = int(input("Enter the number: "))
+# i=1
+# while i<11:
+#     print(f"2 X {i} = {n * i}")
+#     i = i + 1
+
+# 4. Write a program to find whether a given number is prime or not.
+# n = int(input("Enter the number: "))
+# ct=0
+# for i in range(1,10):
+#   if n%i==0:
+#     ct = ct+1  
+# if(ct==2):
+#   print("Prime")            
+# else:
+#   print("not prime")  
+
+# 5. Write a program to find the sum of first n natural numbers using while loop. 
+# *****By using formula*****
+# n = int(input("Enter the number: "))
+# print((n*(n+1))//2)
+# sum=0
+# i=1
+# while i<=n:
+#     sum = sum + i
+#     i = i + 1
+# print(sum)   
+
+# check 6. Write a program to calculate the factorial of a given number using for loop. 
+# n = int(input("Enter a number: "))
+# fact = 1
+# for i in range(1, n + 1):
+#     fact = fact * i
+# print("Factorial =", fact)
+
+# 7. Write a program to print the following star pattern. 
+# * 
+# *** 
+# ***** for n = 3 /
+# <<<Mantra!!>>>
+# 1.Nested Loops
+# 2.Outer loops = number of rows
+# 3.inner loops = focus on column,how to connect column with rows
+# i=1
+# for i in range(1,4):
+#     for j in range(1,2*i):
+#          print("*",end="")
+#     print()        
+
+# 8. Write a program to print the following star pattern: 
+# * 
+# ** 
+# ***      for n = 3
+# for i in range(1,4):
+#      for j in range(1,i+1):
+#         print("*",end="")
+#      print()    
+
+# 9. Write a program to print the following star pattern. 
+# * * * 
+# *   *   for n = 3 
+# * * 
+# n = 3
+# for i in range(1, n + 1):
+#     for j in range(1, n + 1):
+#         if i == 1 or i == n or j == 1 or j == n:
+#             print("*", end=" ")
+#         else:
+#             print(" ", end=" ")
+    # print()
+# 10. Write a program to print multiplication table of n using for loops in reversed 
+# order. 
+# n=2
+# i=10
+# for i in range(10,0,-1):
+# while i>0:
+    # print(f"{n} x {i} = {n*i}")
+    # i=i-1
--- chapter8/eighth.py
+++ chapter8/eighth.py
@@ -1,1 +1,73 @@
 # <<<<<<<<<<Functions & Recursions>>>>>>>>>>
+# 1. Write a program using functions to find greatest of three numbers. 
+# 2. Write a python program using function to convert Celsius to Fahrenheit. 
+# 3. How do you prevent a python print() function to print a new line at the end. 
+# 4. Write a recursive function to calculate the sum of first n natural numbers. 
+# 5. Write a python function to print first n lines of the following pattern: 
+# *** 
+# **               
+# * - for n = 3 
+# 6. Write a python function which converts inches to cms. 
+# 7. Write a python function to remove a given word from a list ad strip it at the same 
+# time. 
+# 8. Write a python function to print multiplication table of a given number. 
+# def greatest(a,b,c):
+#     if a>b and a>c:
+#         return a
+#     elif b>c:
+#         return b
+#     else:
+#         return c
+# print(greatest(4,-1,99))    
+
+# def celsius_to_fahrenheit(celsius):
+#     fahrenheit = (celsius * 9/5) + 32
+#     return fahrenheit
+
+# # Taking user input
+# c = float(input("Enter temperature in Celsius: "))
+# f = celsius_to_fahrenheit(c)
+# print(f"{c}°C = {f}°F")
+
+# 👉 Use print(..., end="") to prevent a newline at the end of the print statement.
+# end="" tells Python not to move to a new line after printing.
+
+# def sum_of_natural_number(num):
+#        if num==0:
+#         return 0
+#        else:
+#            return num + sum_of_natural_number(num - 1)
+            
+# print(sum_of_natural_number(5))
+
+# def print_pattern(n):
+#     for i in range(n, 0, -1):   # start from n down to 1
+#         print("*" * i) → repeats * i times on each line
+
+# print_pattern(3)
+
+# def inches_to_cm(inches):
+#     cm = inches * 2.54
+#     return cm
+
+# # Taking user input
+# inches = float(input("Enter length in inches: "))
+# print(f"{inches} inches = {inches_to_cm(inches)} cm")
+
+# def remove_and_strip(word_list, word_to_remove):
+#     cleaned_list = []
+#     for item in word_list:
+#         item = item.strip()       # remove spaces
+#         if item != word_to_remove:  # skip the given word
+#             cleaned_list.append(item)
+#     return cleaned_list
+
+# words = ["  apple  ", "banana", "  mango  ", "banana  "]
+# result = remove_and_strip(words, "banana")
+# print(result)
+
+# def table(n):
+#     print(f"table of {n}:")
+#     for i in range(1,11):
+#         print(f"{n} x {i} = {n*i}")
+# print(table(2))        
